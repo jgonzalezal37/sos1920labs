64c855109a395eb3b37c264a6afdad2cf38618ea
Jose Manuel <jgonzalezal37@gmail.com>
2020-03-06 11:19:20 +0000

    clase l05

---
+++ l05/index.js
@@ -0,0 +1,14 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+var app = express();
+
+app.get("/cool",(request,response) => {
+	response.send("<html>"+cool()+"</html>");
+});
+
+app.listen(8080, () => {
+	console.log("Server ready")
+});
+
+console.log("server ready...");
