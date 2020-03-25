d740173e6a64297b72758e9f82f81c02bca4facb
Jose Manuel <jgonzalezal37@gmail.com>
2020-03-25 13:08:31 +0000

    realizacion del video l07

--- l05/index.js
+++ l05/index.js
@@ -1,47 +1,77 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 var app = express();
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 80;
 
-app.use("/",express.static("./public"));
-
-app.get("/cool",(request,response) => {
-	response.send("<html>"+cool()+"</html>");
-});
-
-
 var contacts = [
-	{
-		name : "peter",
-		phone : 12345678
+	{ 
+		name: "peter",
+		phone: 123456	
 	},
-	{
+	{ 
 		name: "pablo",
-		phone: 76898789
+		phone: 789456	
 	}
-	
 ];
 
-const BASE_API_URL = "/api/url";
+const BASE_API_URL = "/api/v1";
 
-app.get(BASE_API_URL + "/contacts", (req,res)=>{
+//GET CONTACTS
+
+app.get(BASE_API_URL+"/contacts", (req,res) =>{
 	res.send(JSON.stringify(contacts,null,2));
 });
-app.post(BASE_API_URL + "/contacts", (req,res)=>{
-	contacts.push(req.body)
-	res.sendStatus(200,"OK");
+//POST CONTACTS
+app.post(BASE_API_URL+"/contacts",(req,res) =>{
+	 
+	var newContact = req.body;
+	if((newContact.name == null) || (newContact.name == null)){
+		res.sendStatus(400,"BAD REQUEST (no name provided)");
+	}else{
+		contacts.push(newContact);
+		res.sendStatus(201,"CREATED");
+	}
 });
 
+//DELETE CONTACTS
 
 
+//GET CONTACTS/xxx
+app.get(BASE_API_URL+"/contacts/:name",(req,res)=>{
+	var name = req.params.name;
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name == name)
+	});
+	if(filteredContacts.length >= 1){
+		res.send(filteredContacts[0])
+	}else{
+		res.sendStatus(404, "CONTACT NOT FOUND")
+	}
+});
+
+//PUT CONTACTS/xxx
+
+
+//DELETE CONTACTS/xxx
+app.delete(BASE_API_URL+"/contacts/:name",(req,res)=>{
+	var name = req.params.name;
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name != name)
+	});
+	if(filteredContacts.length < contacts.length){
+		contacts = filteredContacts;
+		res.sendStatus(200, "OK");
+	}else{
+		res.sendStatus(404, "CONTACT NOT FOUND")
+	}
+});
 
 app.listen(port, () => {
 	console.log("Server ready");
 });
 
-console.log("Starting server...hey")
+console.log("Starting server...");
